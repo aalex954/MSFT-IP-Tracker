ed534030ca1fa75f565c4e2f0d0a96a4e004a61d
Alex Fronteddu <alex.fronteddu@pm.me>
2023-04-15 22:52:31 -0400

    Update get_msft_ips.ps1

--- get_msft_ips.ps1
+++ get_msft_ips.ps1
@@ -6,14 +6,12 @@ $current_asn = $as_first
 $url = "https://stat.ripe.net/data/announced-prefixes/data.json?resource=AS$current_asn"
 
 for ($current_asn; $current_asn -le $as_last; $current_asn++) {
-
     $response = Invoke-RestMethod -Uri $url -Method Get
-
+    
     if ($response.status -eq "ok") {
         $prefixes = $response.data.prefixes
-
         Write-Host ";$current_asn"
-
+        
         foreach ($prefix in $prefixes) {
             $($prefix.prefix) | Out-File -FilePath "msft_asn_ip_ranges.txt" -Encoding utf8 -Append
         }
